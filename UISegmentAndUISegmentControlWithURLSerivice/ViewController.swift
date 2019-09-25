2f1e3659424f7d0298f993214501ba4aaa833b0c
Vadde Narendra <narendraiosd@achiever-110.local>
2019-09-25 11:40:15 +0530

    Initial Commit

---
+++ UISegmentAndUISegmentControlWithURLSerivice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISegmentAndUISegmentControlWithURLSerivice
+//
+//  Created by Vadde Narendra on 9/25/19.
+//  Copyright © 2019 Vadde Narendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
